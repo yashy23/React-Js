d4011b2ddb2c880f231184bcf4520f9dde289ff1
Yash S Thakkar <yashthakkar215@gmail.com>
2020-09-23 12:06:48 -0400

    Create dataservice.js

---
+++ src/api/dataservice.js
@@ -0,0 +1,108 @@
+export default function() {
+  return Promise.resolve(
+    [
+        {
+          custid: 1,
+          name: "John",
+          amt: 120,
+          transactionDt: "05-01-2020"
+        },
+        {
+          custid: 1,
+          name: "John",
+          amt: 75,
+          transactionDt: "05-21-2020"
+        },
+        {
+          custid: 1,
+          name: "John",
+          amt: 94,
+          transactionDt: "05-21-2020"
+        },
+        {
+          custid: 1,
+          name: "John",
+          amt: 10,
+          transactionDt: "06-01-2020"
+        },
+        {
+          custid: 1,
+          name: "John",
+          amt: 75,
+          transactionDt: "06-21-2020"
+        },
+        {
+          custid: 1,
+          name: "John",
+          amt: 200,
+          transactionDt: "07-01-2020"
+        },
+        {
+          custid: 1,
+          name: "John",
+          amt: 1,
+          transactionDt: "07-04-2020"
+        },
+        {
+          custid: 1,
+          name: "John",
+          amt: 80,
+          transactionDt: "07-03-2020"
+        },
+        {
+          custid: 1,
+          name: "John",
+          amt: 224,
+          transactionDt: "07-21-2020"
+        },
+        {
+          custid: 2,
+          name: "Bob",
+          amt: 125,
+          transactionDt: "05-01-2020"
+        },
+        {
+          custid: 2,
+          name: "Bob",
+          amt: 75,
+          transactionDt: "05-21-2020"
+        },
+        {
+          custid: 2,
+          name: "Bob",
+          amt: 10,
+          transactionDt: "06-01-2020"
+        },
+        {
+          custid: 2,
+          name: "Bob",
+          amt: 75,
+          transactionDt: "06-21-2020"
+        },
+        {
+          custid: 2,
+          name: "Bob",
+          amt: 200,
+          transactionDt: "07-01-2020"
+        },
+        {
+          custid: 2,
+          name: "Bob",
+          amt: 224,
+          transactionDt: "07-21-2020"
+        },
+        {
+          custid: 3,
+          name: "sai",
+          amt: 120,
+          transactionDt: "07-21-2020"
+        },
+        {
+          custid: 3,
+          name: "sai",
+          amt: 10,
+          transactionDt: "07-21-2020"
+        }
+    ]
+  );
+};
